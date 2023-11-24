64c10de0be7de7060109520c424a846c0b450bde
joshuagrisham-karolinska <106231271+joshuagrisham-karolinska@users.noreply.github.com>
2023-11-24 17:25:57 +0100

    Fix wsl-vpnkit after latest KAROS GPO update

--- install-vpnkit.ps1
+++ install-vpnkit.ps1
@@ -8,3 +8,11 @@ wget https://github.com/sakai135/wsl-vpnkit/releases/download/v0.4.1/wsl-vpnkit.
 
 # Import the tar to a new WSL instance called wsl-vpnkit
 wsl --import wsl-vpnkit --version 2 $env:USERPROFILE\.wsl\wsl-vpnkit $env:USERPROFILE\.wsl\wsl-vpnkit\wsl-vpnkit.tar.gz
+
+# enable [automount] in wsl.conf for wsl-vpnkit distro and kill it so that it will restart correctly
+wsl --distribution wsl-vpnkit --cd /app sed -i '/^\[automount\]$/,/^\[/ s/^enabled[ ]\{0,\}=.*/enabled=true/' /etc/wsl.conf
+wsl --terminate wsl-vpnkit
+
+# copy wsl-gvproxy.exe to C:\git (otherwise it will be blocked by AppLocker)
+mkdir "C:\git\wsl-vpnkit"
+wsl --distribution wsl-vpnkit cp /app/wsl-gvproxy.exe /mnt/c/git/wsl-vpnkit/
